885897fc68fa969ba050c235cd144e8041e19d41
Dilan Nur KAYA <kayadilannur@gmail.com>
2025-04-25 11:10:52 +0300

    Add files via upload

---
+++ Homework1.kt
@@ -0,0 +1,23 @@
+fun main() {
+   var sehir ="urfa"
+   var siparisAdeti =96
+    var renkKodu = 78945
+    var otobusNo = 78
+    var odemeTarihi = "12 nisan"
+    var tatilGunu = "pazartesi"
+    var yemekFiyatı = 95
+    var marka ="Lufian"
+    var muzikAdı ="lovely"
+    var meslek ="software engineering"
+
+    println(sehir)
+    println(siparisAdeti)
+    println(renkKodu)
+    println(otobusNo)
+    println(odemeTarihi)
+    println(tatilGunu)
+    println(yemekFiyatı)
+    println(marka)
+    println(muzikAdı)
+    println(meslek)
+}
